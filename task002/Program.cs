764befd4d128a667751a4908365e01851a339442
Egor Sazhnev <hard-link@mail.ru>
2023-01-12 21:23:21 +1000

    create task002

---
+++ task002/Program.cs
@@ -0,0 +1,22 @@
+﻿/*Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12 */
+System.Console.WriteLine("Введите число: ");
+int A = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine($"Сумма цыфр числа {A} Равняется {GetSumFigure(A)}");
+
+//---------------Method--------------
+int GetSumFigure(int limit)
+{
+    int Sum = 0;
+    int Figure = 0;
+       while (limit > 0)
+    {
+                Figure = limit % 10;
+                limit = limit / 10;
+                Sum = Sum +  Figure;
+    }
+    return Sum;
+}
+
